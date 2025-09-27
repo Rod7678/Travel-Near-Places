a4cdbe15e3c0c645b4ef40c35f7e9dc5e8d264bf
Rohit <rohitghadge348@gmail.com>
2025-09-27 23:50:59 +0530

    add styling to images

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Places from './components/Places.jsx'
 
 function App() {
 
+
   return (
     <>
     <header>
@@ -13,9 +14,17 @@ function App() {
       <p>Create your personal collection of places you would like to visit or you have visited.</p>
     </header>
     <main>
-      <Places title="I'd like to visit . . . ">
+      <Places title="I'd like to visit . . . "
+      places={[]}
+      fallbackText={'Select the places you would like to visit below.'}
+       
+      >
       </Places>
-      <Places title="Available Places">
+      <Places 
+      title="Available Places"
+      places={AVAILABLE_PLACES}
+      fallbackText={'Showing places according to places..'}
+      >
       </Places>
     </main>
     </>
